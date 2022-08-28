b7994257e6450ce53d28c5a84805205b30370563
Brooke Pauly <bpauly@unc.edu>
2022-08-28 16:35:25 -0400

    chardle commit 9?

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -17,19 +17,19 @@ if len(user_letter) != 1:
 if len(user_word) == 5 and len(user_letter) == 1:
     print("Searching for " + user_letter + " in " + user_word)
     if user_word[0] == user_letter:
-        instances: int = instances + 1 
+        instances += 1 
         print((user_letter) + (" found at index 0"))
     if user_word[1] == user_letter:
-        instances: int = instances + 1
+        instances += 1
         print((user_letter) + (" found at index 1"))
     if user_word[2] == user_letter:
-        instances: int = instances + 1
+        instances += 1
         print((user_letter) + (" found at index 2"))
     if user_word[3] == user_letter:
-        instances: int = instances + 1
+        instances += 1
         print((user_letter) + (" found at index 3"))
     if user_word[4] == user_letter:
-        instances: int = instances + 1
+        instances += 1
         print((user_letter) + (" found at index 4"))
 
 if instances >= 1:
