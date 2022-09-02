a700d2e3d9cb9b7e18e7e52689bc4431acf2cf07
Brooke Pauly <bpauly@unc.edu>
2022-09-01 22:12:00 -0400

    test

---
+++ sandbox/testing_for_fun.py
@@ -0,0 +1,12 @@
+"""Genuinely just using this space to type out certain characters without worry"""
+
+__author__ = "730574853"
+
+
+print(ord("C"))
+
+print(chr(129313))
+
+print("110 is on a \U0001F6A2")
+
+print("Hello\nworld\n!!!")
