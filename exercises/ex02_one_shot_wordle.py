ef913a3464a3df56653f353bb069251518e3437e
Brooke Pauly <bpauly@unc.edu>
2022-09-01 22:26:59 -0400

    oswordle commit 3

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -9,7 +9,10 @@ guess: str = input(f"What is your {len(secret_word)}-letter guess? ")
 while len(guess) != len(secret_word):
     guess: str = input(f"That was not {len(secret_word)} letters! Try again: ")
 
-
+if guess == secret_word:
+    print("Woo! You got it!")
+else:
+    print("Not quite. Play again soon!")
 
 
 
