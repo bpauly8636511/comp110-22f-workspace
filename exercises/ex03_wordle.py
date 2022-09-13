f674250fbd13168cb52e261c71f2f899c275dc9c
Brooke Pauly <bpauly@unc.edu>
2022-09-13 15:55:10 -0400

    final wordle

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -2,7 +2,6 @@
 
 __author__ = "730547853"
 
-wordle_emojis: str = (" ")
 
 """Comparing the indices of two variables to see if the second variable is within the first."""
 def contains_char(first_word: str, letter: str) -> bool:
@@ -13,24 +12,25 @@ def contains_char(first_word: str, letter: str) -> bool:
             return True
         else: 
             i += 1
-    if i >= len(first_word) and not True:
-        return False
+    return False
 
 """Codifying the guess to display the colors corresponding to what would happpen in actual wordle."""
 def emojified(guess: str, secret: str) -> str:
+    wordle_emojis: str = (" ")
     i: int = 0
     assert len(guess) == len(secret)
     while i < len(secret):
         if ord(guess[i]) == ord(secret[i]):
             wordle_emojis += "\U0001F7E9"
             i += 1
-        elif contains_char:
+        elif contains_char(secret, guess[i]):
             wordle_emojis += "\U0001F7E8"
             i += 1
         else: 
             wordle_emojis += "\U00002B1C"
             i += 1
-
+    return wordle_emojis
+"""M"""
 def input_guess(expected_length: int) -> str:
     guess: str = input(f"Enter a {expected_length} character word: ")
     while expected_length != len(guess):
@@ -45,13 +45,16 @@ def main() -> None:
     won: bool = False
     while turns <= 6 and not won:
         print(f"== Turn {turns}/6 ==")
-        input_guess(5)
-        emojified(guess, secret)
-        print(emojified)
-        if ord(guess) == ord(secret):
+        guess: str = input_guess(5)
+        print(emojified(guess, secret))
+        if guess == secret:
             won = True
         else:
             turns += 1
+        if won:
+            print(f"You won in {turns}/6 turns!")
+    if not won:
+        print(f"X/6 - Sorry, try again tomorrow!")
 
 if __name__ == "__main__":
     main()
