aa9145d7bdf8941ccd5024b4396ac0cce5897f39
Brooke Pauly <bpauly@unc.edu>
2022-09-19 13:57:44 -0400

    videolesson

--- lessons/sum_test.py
+++ lessons/sum_test.py
@@ -1,5 +1,16 @@
-"""Example of writing a test subject."""
+"""tests for the sum function."""
 
-def sum(xs: list[float]) -> float:
-    """compute the sum of a list."""
-    return 0.0
+from lessons.sum import sum
+
+def test_sum_empty() -> None:
+    xs: list[float] = []
+    assert sum(xs) == 0.0
+
+def test_sum_single_item() -> None:
+    xs: list[float] = [110.0]
+    assert sum(xs) == 110.0
+
+def test_sum_many_items() -> None:
+    xs: list[float] = [1.0, 2.0, 3.0]
+    assert sum(xs) == 6.0
+
