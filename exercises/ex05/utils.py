73d819b927041967ae668bcdc6006033332da864
Brooke Pauly <bpauly@unc.edu>
2022-09-26 19:12:13 -0400

    evensdone

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -2,3 +2,16 @@
 
 __author__ = "730574853"
 
+def only_evens(x: list[int]) -> int:
+    """This takes a list of ints and returns the even numbers in the list."""
+    i: int = 0
+    list_of_evens: int = list()
+    for int in len(x):
+        if x[i] % 2 == 0:
+            list_of_evens += x[i]
+            i += 1
+        else:
+            i += 1
+
+
+def concat(x: list[int], y: list[int]) -> int:
