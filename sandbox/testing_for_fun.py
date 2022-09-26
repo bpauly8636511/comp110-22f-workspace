b66ebc117dff352f0b8799bbfe98c4f37ec70fec
Brooke Pauly <bpauly@unc.edu>
2022-09-26 18:56:26 -0400

    saving

--- sandbox/testing_for_fun.py
+++ sandbox/testing_for_fun.py
@@ -1,12 +1,3 @@
-"""Genuinely just using this space to type out certain characters without worry"""
+i: list[int] = 0, 3, 5
 
-__author__ = "730574853"
-
-
-print(ord("C"))
-
-print(chr(129313))
-
-print("110 is on a \U0001F6A2")
-
-print("Hello\nworld\n!!!")
+print(i)
