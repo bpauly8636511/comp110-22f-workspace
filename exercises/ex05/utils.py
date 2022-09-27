065485199f7d363d4d731d98520188844734e0ea
Brooke Pauly <bpauly@unc.edu>
2022-09-27 15:39:04 -0400

    first test of utils

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -15,3 +15,25 @@ def only_evens(x: list[int]) -> int:
 
 
 def concat(x: list[int], y: list[int]) -> int:
+    """This function takes two lists and adds the second to the end of the first."""
+    concatenated_list: int = list()
+    concatenated_list += x
+    concatenated_list += y
+    return concatenated_list
+
+
+def sub(x: list[int], y: int, z: int) -> int:
+    """This function takes a subset of the list inbetween the indexes given."""
+    sub_list: int = list()
+    i: int = y
+    if y < 0:
+        i: int = 0
+        z -= 1
+    elif z > len(x):
+        z = len(x) - 1
+    elif len(x) == 0 or y > len(x) or z <= 0:
+        return sub_list
+    while i <= z:
+        sub_list += x[i]
+        i += 1
+    return sub_list
