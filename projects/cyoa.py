08fea78b5d473ee75dd7f5a585492ff4099e1650
Brooke Pauly <bpauly@unc.edu>
2022-10-07 11:34:00 -0400

    greet

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -4,6 +4,7 @@ __author__ = "730574853"
 
 POINTS: int = 0
 PLAYER: str = ""
+NEKO: str = ("")
 
 def main() -> None:
     greet()
@@ -14,6 +15,17 @@ def greet() -> None:
     #please add welcome message
     global PLAYER_NAME 
     PLAYER += input(str("What is your name? "))
+    global NEKO
+    NEKO = input("Please choose Neko 1: " + /U000200D + "or Neko 2: " + /U0001F408 + "by entering 1 or 2")
+    while NEKO != 1 or 2:
+        NEKO = input("Please enter 1 or 2 for Neko 1 or Neko 2: ")
+    if NEKO == 1:
+        NEKO = /U000200D
+    else: 
+        NEKO = /U0001F408
+    print(f"Congrats on your new Neko No Tomodachi! {NEKO} Take good care of them!")
+    
+
 
 if __name__ == "__main__":
     main()
