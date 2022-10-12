45055eb76845af6b05d57c42b1cdf2e4eb4e9084
Brooke Pauly <bpauly@unc.edu>
2022-10-12 18:28:01 -0400

    statictype

--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -3,7 +3,7 @@
 __author__ = "730574853"
 
 from dictionary import count, invert, favorite_color
-
+import pytest
 
 def test_invert() -> None:
     """The purpose of this is to do a case test to make sure the function works."""
@@ -13,6 +13,10 @@ def test_invert() -> None:
 
 def test_key_error() -> None:
     """The purpose of this is to do an edge case to make sure it raises a key error for repeat keys."""
+    with pytest.raises(KeyError):
+        my_dictionary = {'kris': 'jordan', 'michael': 'jordan'}
+        invert(my_dictionary)
+
 
 
 def test_invert_large_strings() -> None:
