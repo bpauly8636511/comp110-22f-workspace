878cbfd14dcbc4997d5376d9a0a449ca28119d6f
Brooke Pauly <bpauly@unc.edu>
2022-10-12 16:49:54 -0400

    errorfix

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -6,22 +6,24 @@ def invert(x: dict[str, str]) -> dict[str, str]:
     """This function takes the keys and values and inverts the two using lookup."""
     inverted = dict()
     for keys in x:
-        inverted.update({x[keys]: keys})
+        inverted[x[keys]] = keys
     return inverted
 
 
 
 def count(x: list[str]) -> dict[str, int]:
     """This function takes a list of strings and counts the occurances of each unique string."""
-    keys: list[str] = ""
-    counter: int = 0
+    keys: list[str] = []
+    counter: int = 1
     final_dict = dict()
     for values in x:
         if values not in keys:
             keys.append(values)
-    values_list: list[int] = ()
+    values_list: list[int] = []
     i: int = 0
     for strings in keys:
+        counter = 0
+        i = 0
         while i < len(x):
             if strings == x[i]:
                 counter += 1
@@ -30,15 +32,15 @@ def count(x: list[str]) -> dict[str, int]:
                 i += 1
         values_list.append(counter)
     for index in range(len(keys)):
-        final_dict.update({keys: values_list})
+        final_dict[keys[index]] = values_list[index]
     return final_dict
 
 
 def favorite_color(x: dict[str, str]) -> list[str]:
     """This function looks at the most common string in a dictionary and returns it."""
-    count_list: list[str] = 0
+    count_list: list[str] = []
     for keys in x:
-        count_list += x[keys] 
+        count_list.append(x[keys]) 
     color_count: dict = count(count_list)
     final_list: list[str] = color_count[0]
     for keys in color_count:
