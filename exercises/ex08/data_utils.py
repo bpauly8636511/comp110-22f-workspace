e51b9a5d0fe322c04ce6e26fa89017eb456deebf
Brooke Pauly <bpauly@unc.edu>
2022-11-03 16:15:00 -0400

    correction

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -9,7 +9,7 @@ from csv import DictReader
 def read_csv_rows(filename: str) -> list[dict[str, str]]:
     """read the rows of a csv into a 'table'."""
     result: list[dict[str, str]] = []
-    file_handle = open(filename, "r'", encoding="utf8")
+    file_handle = open(filename, "r", encoding="utf8")
     csv_reader = DictReader(file_handle)
     for row in csv_reader:
         result.append(row)
